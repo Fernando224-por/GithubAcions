5e5545fbfd3b703545a464a972c19a9c1dbe55d3
Fernando <andresfernandoxd1591@gmail.com>
2023-12-23 19:21:15 -0500

    retry deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base:'/gh-pages-ga/',
+  plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true
